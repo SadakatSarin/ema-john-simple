58c95758b3e92a85ff0384f9fee45de8251e336b
unknown <SadakatSarin>
2024-01-28 22:46:17 +0600

    2nd

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './Product.css'
+const Product = (props) => {
+
+    console.log(props.product);
+
+    const {img,name,seller,quantity,price,ratings } = props.product;
+
+
+
+    return (
+        <div className='product'>
+
+
+            <img src={img} alt="" />
+            
+            <div className='product-info'>
+                
+            <h6 className='product-name'>{name}</h6>
+            <p className='product-price'>Price:${price}</p>
+
+            <p className='product-seller'>Manufacturer:{seller}</p>
+            <p className='product-ratings'>Rating:{ ratings}</p>
+            </div>
+
+            <button className='btn-cart'>Add to Cart</button>
+            
+        </div>
+    );
+};
+
+export default Product;
